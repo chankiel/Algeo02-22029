6d367c038c12f12da0a7254af493f62d6b9ecb9e
Marvin Scifo <scifohutahaean@gmail.com>
2023-11-09 01:27:17 +0700

    v.1.1 Front-End Second look

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./../../pages/home/Home";
-import Reservation from "./../../pages/reservation/Reservation";
-import Admin from "./../../pages/admin/admin";
+import Upload from "./../../pages/upload/upload";
+import About from "./../../pages/about/about";
 
 const Routess = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/reservation" element={<Reservation />} />
-                <Route path="/admin" element={<Admin />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/upload" element={<Upload />} />
                 <Route path="*" element={<Home />} />
             </Routes>
         </Router>
